b7c857d7b6229cc343695b8851aa820ef041406f
Pardeep Singh <pard6508@gmail.com>
2026-09-18 14:02:53 +0530

    Some change in backend server

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./Config/db");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const startServer = async () => {
   await connectDB();
